a6b8bbaf16dbdda23ef9f0b2d9c13e8e4b65076a
steffi <stefanie@reneschmidt.de>
2016-07-20 12:18:48 +0200

    modified project to a require.js module (AMD)

---
+++ js/lib/main.js
@@ -0,0 +1,26 @@
+/**
+ * Created by steffi on 20.07.16.
+ */
+
+define([
+
+], function () {
+  /**
+   * Constructor
+   * @constructor
+   */
+  function Main() {
+    var self = this;
+
+    self.init();
+  }
+
+  /**
+   * Init main
+   */
+  Main.prototype.init = function () {
+    var self = this;
+  };
+
+  return (new Main());
+});
---
+++ js/require-setup.js
@@ -0,0 +1,10 @@
+/**
+ * Created by steffi on 20.07.16.
+ */
+
+var require = {
+  baseUrl: 'js',
+  paths: {
+    'main': 'lib/main'
+  }
+};
